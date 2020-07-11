3ef9f67d0b978de029d78d3bed1aff97776199f8
UrsuGheorghe <gheorgheursu@gmail.com>
2020-07-11 19:28:23 +0300

    Implemented the readFile and log how many record we got from csv file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
 import database.DDL;
 import database.SQLiteConnection;
+import model.Customer;
+import service.CsvService;
+import service.CsvServiceImpl;
 
 public class Main {
     private static final SQLiteConnection SQLITE_CONNECTION = new SQLiteConnection();
     private static final String URL = "jdbc:sqlite:D:/sqlite/in_memory.db";
+    private static final String CSV_FILE = "file.csv";
 
     public static void main(String[] args) {
 
         SQLITE_CONNECTION.createTable(DDL.createTableQuery, URL);
+
+        CsvService<Customer> service = new CsvServiceImpl();
+        service.readFile(CSV_FILE);
     }
 }
